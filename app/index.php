e58b591684994623024f0bdbe06f7ae19769461b
Till Kahlbrock <till.kahlbrock@gmx.com>
2014-01-21 08:35:37 +0100

    wip: sending document to couchdb

--- app/index.php
+++ app/index.php
@@ -1,43 +1,51 @@
 <?php
 require '../vendor/autoload.php';
 
-//$options = array(
-//    'dbname' => 'affili8',
-//    'host' => '172.17.0.97', //TODO read from ENV
-//    'port' => 5984, //TODO read from ENV
-//);
-//
-//$client = \Doctrine\CouchDB\CouchDBClient::create($options);
-//
-//list($id, $rev) = $client->postDocument(array('foo' => 'bar'));
-//$client->putDocument(array('foo' => 'baz'), $id, $rev);
-//
-//$doc = $client->findDocument($id);
-//
-//print_r($doc);
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
-$app->get('/affiliates/{name}', function($name) {
-        return 'Hello ' . $name;
-    });
-
-$app->post('/affiliates', function() use($app) {
-        $affiliate = create_new_affiliate($app->json());
-        $affiliateId = $affiliate->getId();
-        return '{"id":"' . $affiliateId . '"}';
+$app->post('/affiliates', function(Request $request) use($app) {
+        $data = $request->getContent();
+        return $data;
+        $repository = new AffiliateRepository(new DatabaseClient());
+        $id = $repository->create($data);
+        return new Response(json_encode(array('id' => $id)), 201);
     });
 
 $app->run();
 
-/**
- * @param $data
- * @return Affiliate
- */
-function create_new_affiliate($data)
+class AffiliateRepository
 {
-    return new Affiliate($data);
+    /**
+     * @var DatabaseClient
+     */
+    private $databaseClient;
+
+    public function __construct(DatabaseClient $databaseClient)
+    {
+        $this->databaseClient = $databaseClient;
+    }
+
+    public function create($data)
+    {
+        $id = $this->databaseClient->store($data);
+        return $id;
+    }
+}
+
+class DatabaseClient
+{
+    public function store($data)
+    {
+        $host = getenv('DB_PORT_5984_TCP_ADDR');
+        $port = getenv('DB_PORT_6379_TCP_PORT');
+        $dbname = 'affili8';
+        $url = $host . ':' . $port . '/' . $dbname;
+        $request = new BCA\CURL\CURL($url);
+        return $request->put($data);
+    }
 }
 
 class Affiliate
